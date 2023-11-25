05f6414f960e9db301405e4640c45d59ad65dbf5
Aide5150 <oakes.alexander207@gmail.com>
2023-11-24 19:22:37 -0500

    a

---
+++ Mods/FullautoLauncher/Harmony/Init.cs
@@ -0,0 +1,15 @@
+﻿using FullautoLauncher.Scripts.ProjectileManager;
+using System.Reflection;
+
+public class FullautoLauncherInit : IModApi
+{
+    public void InitMod(Mod _modInstance)
+    {
+        Log.Out(" Loading Patch: " + GetType());
+        var harmony = new HarmonyLib.Harmony(GetType().ToString());
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        ModEvents.UnityUpdate.RegisterHandler(CustomProjectileManager.Update);
+    }
+}
+
